5b4b5038eade8468a7233b3658d41eecc399c53b
Erik Bernheim <ebb.7897@gmail.com>
2020-04-26 14:17:03 -0400

    removed hashurls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GitService } from './git.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(private route: ActivatedRoute, private git: GitService){}
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      if(params.code){
+        this.git.getGithubToken(params.code).subscribe( ret => {
+          if (!ret.toString().includes('error')) {
+            localStorage.setItem('gitToken', 'Bearer ' + ret.toString().match(/(?<==)(.*?)(?=&)/)[0]);
+            window.close();
+          } else {
+            localStorage.setItem('gitToken', 'ERROR');
+            window.close();
+          }
+        });
+      }
+    });
+  }
   title = 'img-labeler';
 }
