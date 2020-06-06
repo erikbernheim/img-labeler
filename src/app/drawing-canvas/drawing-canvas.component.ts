9953b8bb4430441bd329d98b2feef4e831d8b2ab
Erik Bernheim <ebb.7897@gmail.com>
2020-06-06 19:11:50 -0400

    Revert "use ngbDropdown for color selection in both top menu and layers menu (#44)"
    This reverts commit 5e6cad25cd22fb854fc5aa7b46557baa49adebeb.

--- src/app/drawing-canvas/drawing-canvas.component.ts
+++ src/app/drawing-canvas/drawing-canvas.component.ts
@@ -13,7 +13,6 @@ import { ActivatedRoute } from '@angular/router';
 import { MaskingService } from '../services/masking.service';
 import { Layer } from '../models/layer';
 import { SvgtopngService } from '../services/svgtopng.service';
-import { environment } from 'src/environments/environment';
 @Component({
     selector: 'app-drawing-canvas',
     templateUrl: './drawing-canvas.component.html',
@@ -105,7 +104,6 @@ export class DrawingCanvasComponent implements OnInit, AfterViewInit {
             this.loadedMask = true;
             this.svg.selectAll('.existingMask').remove();
             const g = this.svg.append('g').attr('class', 'existingMask' + ' completePoly').attr('layerHidden', 'false')
-                .attr('color', 'existingMask')
                 .attr('opacity', this.opacity)
                 .attr('visibility', 'visible');
             g.append('svg:image')
@@ -139,9 +137,6 @@ export class DrawingCanvasComponent implements OnInit, AfterViewInit {
             }
             if (obj.type === 'toggleAll') {
                 this.toggleAll();
-            }
-            if (environment.colors.map(e => e.color).includes(obj.type)) {
-                this.updateColor(obj.index, obj.type);
             }
             this.setLayers();
         })
@@ -191,7 +186,7 @@ export class DrawingCanvasComponent implements OnInit, AfterViewInit {
             .attr('cy', this.points[this.points.length - 1][1])
             .attr('r', 4 / (this.panzoomModel.zoomLevel / 1.5))
             .attr('stroke-width', 1 / (this.panzoomModel.zoomLevel / 2))
-            .attr('fill', this.maskSvc.currentColor.color)
+            .attr('fill', 'yellow')
             .attr('stroke', '#000')
             .attr('is-handle', 'true')
             .attr('class', 'inProgressCircle')
@@ -206,7 +201,6 @@ export class DrawingCanvasComponent implements OnInit, AfterViewInit {
         this.svg.selectAll('circle').attr('cursor', 'move');
         this.svg.select('g.drawPoly').remove();
         const g = this.svg.append('g').attr('class', this.maskSvc.currentColor.color + ' completePoly' + child).attr('layerHidden', 'false')
-            .attr('color', this.maskSvc.currentColor.color)
             .attr('opacity', this.opacity)
             .attr('visibility', 'visible');
         g.append('polygon')
@@ -373,12 +367,6 @@ export class DrawingCanvasComponent implements OnInit, AfterViewInit {
 
     public toBottom(i: number) {
         this.artboard.nativeElement.children[0].prepend(this.artboard.nativeElement.children[0].children[i]);
-    }
-
-    public updateColor(i: number, color: string) {
-        this.artboard.nativeElement.children[0].children[i].children[0].setAttribute('style', 'fill: ' + color + ';');
-        this.artboard.nativeElement.children[0].children[i].setAttribute('color', color);
-        // TODO update local storage?
     }
 
     public toggleVisibility(i: number) {
