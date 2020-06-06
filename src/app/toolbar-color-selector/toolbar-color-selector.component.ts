613e715c27b54131d87dc1a62d3e433aa998a3d0
Erik Bernheim <erikbernheim@mail.usf.edu>
2020-06-06 19:12:01 -0400

    Revert "use ngbDropdown for color selection in both top menu and layers menu (#44)" (#45)
    This reverts commit 5e6cad25cd22fb854fc5aa7b46557baa49adebeb.

--- src/app/toolbar-color-selector/toolbar-color-selector.component.ts
+++ src/app/toolbar-color-selector/toolbar-color-selector.component.ts
@@ -10,54 +10,21 @@ import { MaskingService } from '../services/masking.service';
 export class ToolbarColorSelectorComponent implements OnInit {
 
   constructor(public maskSvc: MaskingService) { }
-  public colors;
-  public ntype = environment.colors[0];
-  public compactDropDownMenu = false;
-
+  public colors = environment.colors;
   ngOnInit(): void {
-    this.selectMenuLayout(window.innerWidth);
   }
-
-  public selectMenuLayout(windowWidth) {
-    if (windowWidth < 1200) {
-      this.compactDropDownMenu = true;
-      this.colors = environment.colors.filter(elem => elem.name != this.ntype.name);
-    } else {
-      this.compactDropDownMenu = false;
-      this.colors = environment.colors;
-    }
-  }
-
   public setColor(index: number){
-    this.ntype = this.colors[index];
-    if (this.compactDropDownMenu) {
-      this.maskSvc.setColor(environment.colors.findIndex(e => e.name === this.ntype.name));
-      this.colors = environment.colors.filter(elem => elem.name != this.ntype.name);
-    } else {
-      this.maskSvc.setColor(index);
-      this.colors = environment.colors;
-    }
+    this.maskSvc.setColor(index);
   }
 
 
   @HostListener('document:keyup', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
-    const KeyMap = ['KeyR', 'KeyL', 'KeyU', 'KeyM', 'KeyC'];
-    if (KeyMap.includes(event.code)) {
-      const index = KeyMap.findIndex(e => e === event.code);
-      if (this.compactDropDownMenu) {
-        const name = environment.colors[index].name;
-        if (name !== this.ntype.name) {
-          this.setColor(this.colors.findIndex(e => e.name === name));
-        }
-      } else {
-        this.setColor(index);
-      }
-    }
+      if (event.code === 'KeyR') { this.setColor(0); }
+      if (event.code === 'KeyL') { this.setColor(1); }
+      if (event.code === 'KeyU') { this.setColor(2); }
+      if (event.code === 'KeyM') { this.setColor(3); }
+      if (event.code === 'KeyC') { this.setColor(4); }
   }
   
-  @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    this.selectMenuLayout(window.innerWidth);
-  }
 }
